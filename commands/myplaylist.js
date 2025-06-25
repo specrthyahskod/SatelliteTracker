f6849cfd099ddd11d877c658eee120e85d12952f
lunarlord74 <hazrapopi96@gmail.com>
2025-06-25 10:41:55 +0530

    Initial commit

--- commands/myplaylist.js
+++ commands/myplaylist.js
@@ -1,6 +1,6 @@
 // commands/myplaylist.js
 const { SlashCommandBuilder } = require('discord.js');
-const generateGeminiPlaylist = require('../utils/geminiPlaylist');
+const generateGeminiPlaylist = require('../utils/gptPlaylist');
 
 module.exports = {
   data: new SlashCommandBuilder()
